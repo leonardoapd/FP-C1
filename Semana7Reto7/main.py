1bab4aabb7b10b4612f42958378c8dd1d1d59658
Leonardo Perdomo <leonardop763@gmail.com>
2021-06-21 11:44:37 -0500

    Comments added

--- Semana7Reto7/main.py
+++ Semana7Reto7/main.py
@@ -5,33 +5,35 @@
 #
 #######################################################
 
-"""
-Así es mi pequeño saltamontes, ya estás en capacidad de hacer el programa competo desde cero.
-no olvidescrear los modulos necesarios, importar las librerías y una función main que de un poco de orden al archivo pincipal
-"""
-
-from numpy import cosh
 import modulo as mod
 import pandas as pnd
 
 
 def main():
+    #Se inicializa una lista con valores simulados del costo de matricula de 10 estudiantes
     costoDeMatricula = [2014000, 1405550, 2300093, 5590000,
         3431212, 5431212, 3231233, 3213123, 2340000, 5202121]
+    #Se hace carga un archivo json y se convierte a dataframe usando la libreria pandas de python
     dataframe = mod.cargarJsonADataFrame("Semana7Reto7/estudiantes.json")
     
-    
+    #Se imprimen los 5 primeros estudiantes incluidos en el dataframe
     print(mod.mostrar5primeros(dataframe))
     print()
+    #Se imprimen los 5 ultimos estudiantes en el dataframe
     print(mod.mostrar5ultimos(dataframe))  
     print()
+    #Se imprime el listado de mayores de 20 años de edad
     print(mod.mayoresDe(dataframe, 20))
     print()
 
+    #Se crea un nuevo listado dataframe basado en el anterior, la funcion pregunta que columnas se quieren incluir
     newDataframe = mod.nuevoDataframe(dataframe)
+    #Se le añade una nueva columna llamada costos y se le agrega la informacion de la lista antes inicializada
     newDataframe2 = mod.agregarColumna(costoDeMatricula, newDataframe)
+    #Se pregunta por los estudiantes que pagaron mas de 5M 
     newDataframe3 = mod.matriculaCostosa(newDataframe2, 5000000)
 
+    #Se imprimen los dataframes antes creados
     print(newDataframe)
     print()
     print(newDataframe2)
--- Semana7Reto7/modulo.py
+++ Semana7Reto7/modulo.py
@@ -1,30 +1,38 @@
 import pandas as pnd
 
+#Funcion para cargar el json y convertirlo a dataframe
 def cargarJsonADataFrame(path):
     dataframe = pnd.read_json(path)
     return dataframe
 
+#Funcion para mostrar los 5 primeros del dataframe
 def mostrar5primeros(dataframe):
     return dataframe.head(5)
 
+#Funcion para mostrar los 5 ultimos del dataframe
 def mostrar5ultimos(dataframe):
     return dataframe.tail(5)
 
+#Funcion para evaluar si dentro del dataframe existe un dato con una edad o mayor a ella
 def mayoresDe(dataframe, edad):
     return dataframe[dataframe["edad"]>=edad]
 
+#Funcion para crear un dataframe con base a otro de entrada, tomando las columnas 
 def nuevoDataframe(dataframe):
     opcion = int(input("Elija la cantidad de columnas que desea mostrar: "))
     lista = [input("Ingrese el nombre de la columna del Dataframe a agregar: ") for i in range(opcion)]
     newDataframe = dataframe[lista]
     return newDataframe
 
+#Funcion para agregar una nueva columna llamada costo con datos de una lista entrante
 def agregarColumna(lista, dataframe):
     newDataframe = dataframe.assign(costo = lista)
     return newDataframe
 
+#Funcion para evaluar si un dato de la lista supera el costo ingresado
 def matriculaCostosa(dataframe, costo):
     return dataframe[dataframe["costo"]>costo]
 
+#Funcion para exportar un archivo excel
 def haciaExcel(dataframe):
     dataframe.to_excel("Semana7Reto7/archivoExcel.xlsx")
